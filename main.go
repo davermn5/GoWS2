04348cb450c37bf8a5cbce7deaa046b4dc8387ba
JR <davermn5@gmail.com>
2023-08-12 18:00:20 -0700

    Adding API with performance enhancements

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Geri Mulligan", Price: 17.99},
+	{ID: "3", Title: "Songs of the Past", Artist: "Miles Davis", Price: 39.99},
+}
+
+func getAlbums(c *gin.Context) {
+	c.JSON(http.StatusOK, albums)
+}
+
+func getAlbumByID(c *gin.Context) {
+	for _, albumRecord := range albums {
+		if c.Param("id") == albumRecord.ID {
+			c.JSON(http.StatusOK, albumRecord)
+			return
+		}
+	}
+	c.JSON(http.StatusNotFound, gin.H{"message": 9})
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.JSON(http.StatusCreated, newAlbum)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
+	router.POST("/albums", postAlbums)
+	router.Run("localhost:8081")
+
+}
